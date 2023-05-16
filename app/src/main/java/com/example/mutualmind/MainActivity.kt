277613018e6a61caf08d8d612b69b012d94e4ce1
Shreyas <shreyaskapse171@gmail.com>
2023-05-17 01:19:34 +0530

    added welcome ui

---
+++ app/src/main/java/com/example/mutualmind/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mutualmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
