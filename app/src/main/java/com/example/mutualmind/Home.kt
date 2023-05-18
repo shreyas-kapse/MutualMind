4787c4e97da7c7aa5c2a1b15fc2b8681855fa876
Shreyas <shreyaskapse171@gmail.com>
2023-05-18 21:07:42 +0530

    added bottom navigation and navigation component,added authentication and email verification

---
+++ app/src/main/java/com/example/mutualmind/Home.kt
@@ -0,0 +1,25 @@
+package com.example.mutualmind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.mutualmind.databinding.ActivityHomeBinding
+
+
+class Home : AppCompatActivity() {
+    lateinit var binding: ActivityHomeBinding
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val hostFragment=supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController=hostFragment.navController
+        val bottom_nav=binding.bottomNavigationView
+        setupWithNavController(bottom_nav,navController)
+
+    }
+}
--- app/src/main/java/com/example/mutualmind/Login.kt
+++ app/src/main/java/com/example/mutualmind/Login.kt
@@ -3,35 +3,73 @@ package com.example.mutualmind
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mutualmind.databinding.ActivityLoginBinding
+import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import java.lang.Exception
 
 class Login : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
-    lateinit var firebaseAuth:FirebaseAuth
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        firebaseAuth=FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance()
         // loading xml data to variables
-        val loginbtn=binding.logLogBtn
-        val forgetpassbtn=binding.logForgetpassTxt
-        val registerbtn=binding.logJoinnowTxt
+        val loginbtn = binding.logLogBtn
+        val forgetpassbtn = binding.logForgetpassTxt
+        val registerbtn = binding.logJoinnowTxt
 
 
         loginbtn.setOnClickListener {
-            val emailedt=binding.logEmailEdt.text.toString()
-            val passedt=binding.logPassEdt.text.toString()
-            if(emailedt.isNotEmpty()&&passedt.isNotEmpty()){
-                firebaseAuth.signInWithEmailAndPassword(emailedt,passedt).addOnCompleteListener {
-                    task->
-                    if(task.isSuccessful){
-                        if(firebaseAuth.currentUser?.isEmailVerified==true){
-//                            val intent=Intent(this,)
+            val emailedt = binding.logEmailEdt.text.toString()
+            val passedt = binding.logPassEdt.text.toString()
+            if (emailedt.isNotEmpty() && passedt.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(emailedt, passedt)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            if (firebaseAuth.currentUser?.isEmailVerified == true) {
+                                val intent = Intent(this, Home::class.java)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this, "Verify your email first.", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        } else {
+                            try {
+                                throw task.exception!!
+                            } catch (e: FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(
+                                    this,
+                                    "Invalid credentials check email and password",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            } catch (e: FirebaseAuthInvalidUserException) {
+                                Toast.makeText(this, "No account found", Toast.LENGTH_SHORT).show()
+
+                            } catch (e: FirebaseAuthUserCollisionException) {
+                                Toast.makeText(
+                                    this,
+                                    "This email is already linked to another account",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                            } catch (e: FirebaseNetworkException) {
+                                Toast.makeText(this, "Network error", Toast.LENGTH_SHORT).show()
+                            } catch (e: Exception) {
+                                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT)
+                                    .show()
+                            }
                         }
                     }
-                }
+            }
+            else{
+                Toast.makeText(this, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
             }
         }
         forgetpassbtn.setOnClickListener {
