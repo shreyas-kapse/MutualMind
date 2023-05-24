26128ec95410d892645b931689e3acb26ef66235
Shreyas <shreyaskapse171@gmail.com>
2023-05-24 23:43:54 +0530

    added searchview to recyclerview

--- app/src/main/java/com/example/mutualmind/Fragments/AddFund.kt
+++ app/src/main/java/com/example/mutualmind/Fragments/AddFund.kt
@@ -1,67 +1,35 @@
 package com.example.mutualmind.Fragments
 
+import VolleySingleTon
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.mutualmind.Adapter.FundInfoAdapter
 import com.example.mutualmind.Model.BasicFundInfo
 import com.example.mutualmind.R
 import com.example.mutualmind.databinding.FragmentAddFundBinding
 import org.json.JSONArray
-
-//class AddFund : Fragment() {
-//    lateinit var binding: FragmentAddFundBinding
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val view=inflater.inflate(R.layout.fragment_add_fund, container, false)
-//        fetchFunds()
-//        val recyclerView = view.findViewById<RecyclerView>(R.id.frag_add_fund_recycler)
-//        recyclerView.layoutManager = LinearLayoutManager(context)
-//
-//        return view
-//    }
-//
-//    private fun fetchFunds() {
-//        val data = ArrayList<BasicFundInfo>()
-//        val url = "https://api.mfapi.in/mf"
-//        val jsonObject = JsonArrayRequest(Request.Method.GET, url, null, Response.Listener {
-//            val jsonArray = JSONArray(it.toString())
-//            Log.d("res ", "$jsonArray")
-//            for (i in 0 until jsonArray.length()) {
-//                val jsonObject = jsonArray.getJSONObject(i)
-//                val schemename = jsonObject.getString("schemeName")
-//                val schemecode = jsonObject.getString("schemeCode")
-//
-//                val fund = BasicFundInfo(schemename, schemecode)
-//                data.add(fund)
-//                binding.fragAddFundRecycler.adapter=FundInfoAdapter(data)
-//            }
-//        }, { error ->
-//            Log.d("res", "$error")
-//        })
-//        VolleySingleTon.getInstance(requireActivity().applicationContext).addToRequestQueue(jsonObject)
-//    }
-//
-//
-//}
+import java.util.Locale
 
 class AddFund : Fragment() {
     private lateinit var binding: FragmentAddFundBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: FundInfoAdapter
-    private val data = ArrayList<BasicFundInfo>()
-
+    private var data = ArrayList<BasicFundInfo>()
+    private var tempdata = ArrayList<BasicFundInfo>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -69,18 +37,47 @@ class AddFund : Fragment() {
         binding = FragmentAddFundBinding.inflate(inflater, container, false)
         recyclerView = binding.fragAddFundRecycler
         recyclerView.layoutManager = LinearLayoutManager(context)
-        adapter = FundInfoAdapter(data)
-        recyclerView.adapter = adapter
+
 
         fetchFunds()
+        tempdata.addAll(data)
+        binding.searchview.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
 
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                tempdata.clear()
+                val searchText=newText!!.lowercase(Locale.getDefault())
+                if(searchText.isNotEmpty()){
+                    data.forEach {
+                        if(it.schemename.lowercase(Locale.getDefault()).contains(searchText)){
+                            tempdata.add(it)
+                        }
+                    }
+                    recyclerView.adapter!!.notifyDataSetChanged()
+                }
+                else{
+                    tempdata.clear()
+                    tempdata.addAll(data)
+                   recyclerView.adapter!!.notifyDataSetChanged()
+                }
+                return false
+            }
+
+        })
+        adapter = FundInfoAdapter(tempdata)
+        recyclerView.adapter = adapter
         return binding.root
     }
 
     private fun fetchFunds() {
+        binding.progressbar.visibility=View.VISIBLE
         val url = "https://api.mfapi.in/mf"
         val jsonObject = JsonArrayRequest(Request.Method.GET, url, null, Response.Listener {
             val jsonArray = JSONArray(it.toString())
+            binding.progressbar.visibility=View.GONE
             Log.d("res ", "$jsonArray")
             for (i in 0 until jsonArray.length()) {
                 val jsonObject = jsonArray.getJSONObject(i)
@@ -90,10 +87,12 @@ class AddFund : Fragment() {
                 val fund = BasicFundInfo(schemeName, schemeCode)
                 data.add(fund)
             }
-            adapter.notifyDataSetChanged()
+            tempdata.addAll(data)
+            recyclerView.adapter!!.notifyDataSetChanged()
         }, { error ->
             Log.d("res", "$error")
         })
-        VolleySingleTon.getInstance(requireActivity().applicationContext).addToRequestQueue(jsonObject)
+        VolleySingleTon.getInstance(requireActivity().applicationContext)
+            .addToRequestQueue(jsonObject)
     }
 }
