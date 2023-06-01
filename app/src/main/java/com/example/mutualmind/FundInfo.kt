06c8ef94b45ca48091fcc1e7c49f4faad595e095
Shreyas <shreyaskapse171@gmail.com>
2023-06-02 01:04:47 +0530

    added performance logic in fundinfo

--- app/src/main/java/com/example/mutualmind/FundInfo.kt
+++ app/src/main/java/com/example/mutualmind/FundInfo.kt
@@ -1,9 +1,11 @@
 package com.example.mutualmind
 
 import VolleySingleTon
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -40,7 +42,8 @@ class FundInfo : AppCompatActivity() {
             val NAV = nav.toLong()
             val investment = amount.toLong()
             val units = investment / NAV.toInt()
-            addDataToDatabase(nav, amount, units.toString(), fundCode.toString(),
+            addDataToDatabase(
+                nav, amount, units.toString(), fundCode.toString(),
                 fundName.toString()
             )
 
@@ -64,11 +67,13 @@ class FundInfo : AppCompatActivity() {
                 firebaseAuth.currentUser?.uid.toString()
             )
         )
-        val myref=FirebaseDatabase.getInstance().getReference(firebaseAuth.currentUser?.uid.toString())
-        myref.child(fundName).setValue(FirebaseDataModel(nav, amount, units, fundName, fundCode)).addOnCompleteListener {
-            binding.funNavEdt.text=null
-            binding.funAmountEdt.text=null
-        }
+        val myref =
+            FirebaseDatabase.getInstance().getReference(firebaseAuth.currentUser?.uid.toString())
+        myref.child(fundName).setValue(FirebaseDataModel(nav, amount, units, fundName, fundCode))
+            .addOnCompleteListener {
+                binding.funNavEdt.text = null
+                binding.funAmountEdt.text = null
+            }
 
     }
 
@@ -89,16 +94,83 @@ class FundInfo : AppCompatActivity() {
                 val info = PriceData(date, nav)
                 priceData.add(info)
             }
+            calculateReturns(priceData)
             val nav = priceData.get(0).price
             binding.funFundName.text = schemeName
             binding.funCategory.text = category
             binding.funFundHouse.text = fundHouse
             binding.funNav.text = nav
-            Log.d("REST RES", " $priceData")
+//            Log.d("REST RES", " $priceData")
         }) { error ->
             Log.d("REST RES", "error $error")
 
         }
         VolleySingleTon.getInstance(applicationContext).addToRequestQueue(jsonObject)
+    }
+
+    private fun calculateReturns(priceData: ArrayList<PriceData>) {
+        val oneWeek = ArrayList<PriceData>()
+        val oneMonth = ArrayList<PriceData>()
+        val threeMonths = ArrayList<PriceData>()
+        val sixMonths = ArrayList<PriceData>()
+        val oneYear = ArrayList<PriceData>()
+        for (i in 0 until 7) {
+            oneWeek.add(priceData.get(i));
+        }
+        for (i in 0 until 30) {
+            oneMonth.add(priceData.get(i))
+        }
+        for (i in 0 until 90) {
+            threeMonths.add(priceData.get(i))
+        }
+        for (i in 0 until 180) {
+            sixMonths.add(priceData.get(i))
+        }
+        for (i in 0 until 365) {
+            oneYear.add(priceData.get(i))
+        }
+        val oneDayBtn=findViewById<Button>(R.id.oneDay)
+        val oneWeekBtn = findViewById<Button>(R.id.oneWeek)
+        val oneMonthBtn = findViewById<Button>(R.id.oneMonth)
+        val threeMonthsBtn = findViewById<Button>(R.id.threeMonths)
+        val sixMonthsBtn = findViewById<Button>(R.id.sixMonths)
+        val oneYearBtn = findViewById<Button>(R.id.oneYear)
+        setTrendColor(oneWeek.get(1).price.toDouble()>oneWeek.get(0).price.toDouble(),oneDayBtn)
+        setTrendColor(
+            oneWeek.get(oneWeek.size - 1).price.toDouble() > oneWeek.get(0).price.toDouble(),
+            oneWeekBtn
+        )
+        setTrendColor(
+            oneMonth.get(oneMonth.size - 1).price.toDouble() > oneMonth.get(0).price.toDouble(),
+            oneMonthBtn
+        )
+        setTrendColor(
+            threeMonths.get(threeMonths.size - 1).price.toDouble() > threeMonths.get(0).price.toDouble(),
+            threeMonthsBtn
+        )
+        setTrendColor(
+            sixMonths.get(sixMonths.size - 1).price.toDouble() > sixMonths.get(0).price.toDouble(),
+            sixMonthsBtn
+        )
+        setTrendColor(
+            oneYear.get(oneYear.size - 1).price.toDouble() > oneYear.get(0).price.toDouble(),
+            oneYearBtn
+        )
+//        if (oneWeek.get(oneWeek.size - 1).price.toDouble() > oneWeek.get(0).price.toDouble()) {
+//            b1.setBackgroundColor(Color.RED)
+//
+//        } else {
+//            b1.setBackgroundColor(Color.parseColor("#18BC28"))
+//        }
+    }
+
+    private fun setTrendColor(b: Boolean, b1: Button) {
+        if (b) {
+            //negative
+            b1.setBackgroundColor(Color.RED)
+        } else {
+            //positive
+            b1.setBackgroundColor(Color.parseColor("#18BC28"))
+        }
     }
 }
