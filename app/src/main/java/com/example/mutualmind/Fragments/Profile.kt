1cb759f19f006b7f5369de66fc6ad29247d985df
Shreyas <shreyaskapse171@gmail.com>
2023-06-09 01:19:47 +0530

    added coroutine for logout operation

--- app/src/main/java/com/example/mutualmind/Fragments/Profile.kt
+++ app/src/main/java/com/example/mutualmind/Fragments/Profile.kt
@@ -1,20 +1,54 @@
 package com.example.mutualmind.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.example.mutualmind.Login
 import com.example.mutualmind.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.suspendCoroutine
+import kotlin.math.log
 
 class Profile : Fragment() {
-
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view=inflater.inflate(R.layout.fragment_profile, container, false)
+        firebaseAuth=FirebaseAuth.getInstance()
+        val logout=view.findViewById<LinearLayout>(R.id.logout)
+        val coroutineScope= CoroutineScope(Dispatchers.IO)
+        logout.setOnClickListener {
+//            Toast.makeText(context, "This is logout btn", Toast.LENGTH_SHORT).show()
+            coroutineScope.launch { signout()}
+        }
+        return view
+    }
+
+    private suspend fun signout(){
+        val coroutineScope= CoroutineScope(Dispatchers.IO)
+        coroutineScope.launch { val signout=async {  firebaseAuth.signOut()}
+        signout.await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(context, "Logout Successful", Toast.LENGTH_SHORT).show()
+                val intent=Intent(context,Login::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 
 }
